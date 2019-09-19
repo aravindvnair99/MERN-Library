fcc3f044df3cfdc0cb7dac2f356988e56b7de57b
Nuthi-Sriram <sriramnuthi@gmail.com>
2019-09-19 21:07:19 +0530

    Added passport and settings

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+var JwtStrategy = require("passport-jwt").Strategy;
+Extractjwt = require("passport-jwt").ExtractJwt;
+
+var User = require("../models/user");
+var Settings = require("../config/setting");
+
+module.exports = function(passport) {
+  var opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+  opts.secretOrKey = settings.secret;
+  passport.use(
+    new JwtStrategy(opts, function(jwt_payload, done) {
+      User.findOne({ id: jwt_payload.id }, function(err, user) {
+        if (err) {
+          return done(err, false);
+        }
+        if (user) {
+          done(null, user);
+        } else {
+          done(null, false);
+        }
+      });
+    })
+  );
+};
--- routes/book.js
+++ routes/book.js
@@ -1,8 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mongoose = require("mongoose");
+var Book = require("../models/Book.js");
 
-router.get('/', function(req, res, next) {
-	res.send('Express REST API');
+router.get("/", function(req, res, next) {
+  Book.find(function(err, products) {
+    if (err) return next(err);
+    res.json(products);
+  });
+});
+
+router.post("/", function(req, res, next) {
+  Book.create(req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
 });
 
 module.exports = router;
